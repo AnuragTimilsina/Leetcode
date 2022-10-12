0e13ad98f1c89bd354a676f742b62d00c4e5e54d
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-10-12 08:50:24 +0545

    Improved pivot array problem's time complexity

--- LeetCode75_L1/find_pivot_of_an_array.py
+++ LeetCode75_L1/find_pivot_of_an_array.py
@@ -1,3 +1,29 @@
+# class Solution(object):
+#     def pivotIndex(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+        
+#         last_index = len(nums)
+#         for i in range(len(nums)):
+            
+#             # Check Leftmost pivot
+#             if i == 0:
+#                 if sum(nums[1:last_index]) == 0:
+#                     return 0
+                
+#             if sum(nums[0:i]) == sum(nums[i+1:last_index]):
+#                 return i
+            
+#         return -1
+
+
+'''
+    The above code worked fine however it had time complexity of O(n^2).
+'''
+
+
 class Solution(object):
     def pivotIndex(self, nums):
         """
@@ -5,15 +31,15 @@ class Solution(object):
         :rtype: int
         """
         
-        last_index = len(nums)
-        for i in range(len(nums)):
-            
-            # Check Leftmost pivot
-            if i == 0:
-                if sum(nums[1:last_index]) == 0:
-                    return 0
-                
-            if sum(nums[0:i]) == sum(nums[i+1:last_index]):
+        Sum = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (Sum - leftsum - x):
                 return i
-            
+            leftsum += x
         return -1
+
+
+'''
+    This logic worked like a charm and had time complexity of O(n) only. 
+'''
