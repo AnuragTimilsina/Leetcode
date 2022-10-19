534ed4289da5225e21740f673fa5daf50c07eb14
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-10-19 09:09:59 +0545

    Fixed indentation in solution3

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -15,9 +15,10 @@ class Solution(object):
                 mapping_s_t[c1] = c2
                 mapping_t_s[c2] = c1
                 
-            # Case 2: Rither mapping doesn't exist in one of the dictionaries or                Mapping exists and it doesn't match in either of the dictionaries or both
+            # Case 2: Rither mapping doesn't exist in one of the dictionaries or                
+            # Mapping exists and it doesn't match in either of the dictionaries or both
             
             elif mapping_s_t.get(c1) != c2 or mapping_t_s.get(c2) != c1:
                 return False
-            return True
+        return True
         
